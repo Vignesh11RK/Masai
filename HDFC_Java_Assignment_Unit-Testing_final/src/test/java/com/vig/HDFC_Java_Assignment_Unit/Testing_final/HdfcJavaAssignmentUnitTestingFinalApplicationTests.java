5b3977a9803ec9a734f5ca911d59ab7b572d5143
Vignesh11RK <kvignesh@hdfclife.com>
2025-09-17 22:47:51 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ HDFC_Java_Assignment_Unit-Testing_final/src/test/java/com/vig/HDFC_Java_Assignment_Unit/Testing_final/HdfcJavaAssignmentUnitTestingFinalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.vig.HDFC_Java_Assignment_Unit.Testing_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdfcJavaAssignmentUnitTestingFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
