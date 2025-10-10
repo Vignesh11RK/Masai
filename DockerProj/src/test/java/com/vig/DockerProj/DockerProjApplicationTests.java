cc0870c2834a17275225adc721f628b88d4f0db5
Vignesh11RK <kvignesh@hdfclife.com>
2025-10-10 17:37:13 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ DockerProj/src/test/java/com/vig/DockerProj/DockerProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vig.DockerProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
