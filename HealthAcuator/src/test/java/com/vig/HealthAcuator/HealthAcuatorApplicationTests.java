2657147211f4cfa648823a757042a9885e687e98
KVIGNESH_hdfclife <kvignesh@hdfclife.com>
2025-10-14 10:23:31 +0530

    mongo crud

---
+++ HealthAcuator/src/test/java/com/vig/HealthAcuator/HealthAcuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vig.HealthAcuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAcuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
