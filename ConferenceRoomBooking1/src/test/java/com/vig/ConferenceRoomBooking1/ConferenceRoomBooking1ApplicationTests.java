90530fd89d373851e7ec47e6008ca6b34f20c480
Vignesh11RK <kvignesh@hdfclife.com>
2025-11-05 12:15:25 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ ConferenceRoomBooking1/src/test/java/com/vig/ConferenceRoomBooking1/ConferenceRoomBooking1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vig.ConferenceRoomBooking1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomBooking1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("✅ Application context loaded successfully!");
+    }
+}
